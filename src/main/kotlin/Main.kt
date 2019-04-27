542b236e962127b150983eeea3661d6d27b69ed0
cbartolomeu <43549@alunos.isel.ipl.pt>
2019-04-27 16:02:27 +0100

    simple proxy

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+import io.ktor.application.Application
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main() {
+    embeddedServer(Netty, 8080, module = Application::simpleProxyModule).start()
+}
